a6d12d8e8506a29ae05cd7048dcf2b2c8e729473
Tianchao Wang <chaowork2018@gmail.com>
2021-04-26 16:45:49 -0700

    Functional Programming

--- encapsulation/tree/traverse.go
+++ encapsulation/tree/traverse.go
@@ -17,3 +17,26 @@ func TraverseInorder(root *Node) {
 	println(root.Value)
 	TraverseInorder(root.Right)
 }
+
+func (node *Node) TraverseFunc(f func(*Node)) {
+	if node == nil {
+		return
+	}
+	node.Left.TraverseFunc(f)
+	f(node) // do sth in function
+	node.Right.TraverseFunc(f)
+}
+
+func (node *Node) Traverse() {
+	node.TraverseFunc(func(n *Node) {
+		n.PrintTreeNodeWithPrefix("")
+	})
+}
+
+func countNode(root *Node) {
+	count := 0
+	root.TraverseFunc(func(n *Node) {
+		count++
+	})
+	println("count = ", count)
+}
